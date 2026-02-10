d8c03e0d46e753c3d35e8f26d089ecb000662af8
Devendra Kumar Prajapat <dkprajapat1989@gmail.com>
2026-02-10 11:58:24 +0530

    Add files via upload

---
+++ arrays-basics/Prime Number.py
@@ -0,0 +1,12 @@
+# Program to check if a number is prime or not
+num = int(input("Enter a number to check: "))
+
+if num > 1:
+    for i in range(2, int(num**0.5) + 1):
+        if (num % i) == 0:
+            print(num, "is not a prime number")
+            break
+    else:
+        print(num, "is a prime number")
+else:
+    print(num, "is not a prime number")
